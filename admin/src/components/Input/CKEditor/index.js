00f1a40fea51b33de243ba6e209a30acda876f86
iamtun <letuan19431791.iuh@gmail.com>
2024-07-26 13:18:26 +0700

    feat: Add CKEditor CSS for inline editing and focused state

--- admin/src/components/Input/CKEditor/index.js
+++ admin/src/components/Input/CKEditor/index.js
@@ -8,6 +8,7 @@ import {getConfiguration} from "./configuration";
 import {getGlobalStyling} from "./styling";
 import MediaLib from "../MediaLib";
 
+import './CKeditor.css';
 import ckeditor5Dll from "ckeditor5/build/ckeditor5-dll.js";
 import ckeditor5EditorClassicDll from "@ckeditor/ckeditor5-editor-classic/build/editor-classic.js";
 
